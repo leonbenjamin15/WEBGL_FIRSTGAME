500a2f1a6ab2c2ace1c3f019f28006e18832fbce
leonbenjamin15 <leonbenjamin15@hotmail.com>
2014-04-02 17:01:04 +0200

    shizzle
    vloer gelegd :) lukt me alleen nog niet om texture toe te voegen
    misschien kan jij dit checken

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // get screen width and height
 var width = window.innerWidth;
 var height = window.innerHeight;
@@ -11,23 +10,23 @@ document.body.appendChild(renderer.domElement);  // renderer add zichzelf in de 
  
 var scene = new THREE.Scene;
 
-var cubeGeometry = new THREE.CubeGeometry(100, 100, 100);	
-var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x0000FF }); // blauw
+var cubeGeometry = new THREE.CubeGeometry(1000, 10, 1000);	
+var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x0000FF })
 var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);	// maak een cube
- 
+  
 cube.rotation.y = Math.PI * 45 / 180;	// draai de cube 45 graden op de Y-as 
  
 scene.add(cube);
 
-var pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(0, 300, 300);
- 
-scene.add(pointLight);
+var pointLight1 = new THREE.PointLight(0xffffff);
+pointLight1.position.set(0, 400, 0);	// licht hang recht boven de scene 
+scene.add(pointLight1);
+
 // dit wordt onze camera
 var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000); //45 graden lens, ziet niet dichterbij dan 0.1 en niet verderweg dan 10000
 
 // zet de camera positie naar boven (y) en iets naar achter (z) zodat we de cube (die in het midden staat) kunnen zien
-camera.position.y = 80;
+camera.position.y = 40;
 camera.position.z = 400;
 
 scene.add(camera); 

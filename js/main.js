a43089df12e5eec5bed6e7a769ce999e98137f12
leonbenjamin15 <leonbenjamin15@hotmail.com>
2014-04-16 18:17:57 +0200

    Main gefixt
    alles is nu getextured sync dit eerst voordat je gaat commiten

--- js/main.js
+++ js/main.js
@@ -15,23 +15,24 @@ renderer.setClearColor(0xFFFFFF, 1);
 document.body.appendChild(renderer.domElement);  // renderer add zichzelf in de webpage, geen <canvas> tag nodig dus
  
 var scene = new THREE.Scene;
-
 var texture = THREE.ImageUtils.loadTexture( 'images/floor_texture01.png' );
 
-=======
-init(width, height);
-
 //geef de player een camera
+
 addCamera(width, height);
-
-//render floor
-addCube(9000, 0, 9000, 0xFFFFFF, 0, 0, 'images/floor_texture01.png', 0x555555, 20, true, 10, 10);
-
+camera.position.z = 0;
+camera.position.x = 0;
+// twee lichten van twee kanten zodat alle zijdes belicht zijn
 var pointLight1 = new THREE.PointLight(0xffffff);
-pointLight1.position.set(0, 400, 600);	// licht hang recht boven de scene 
+pointLight1.position.set(0, 400, 600);	// licht hang recht boven en voor de scene 
 scene.add(pointLight1);
+var pointLight2 = new THREE.PointLight(0xffffff);
+pointLight2.position.set(0, 400, -600);	// licht hang recht boven en achter de scene 
+scene.add(pointLight2);
  
-addCube(30,30,30, 0xFF0000, 0, 330, 'images/floor_texture01.png', 0x555555, 20);
+addCube(30,30,30, texture, 0, 0, -200);
+//render floor
+addCube(500, 10, 500, texture, 0, -10, 0); // zet de vloer -10 naar beneden zodat alles op de vloer op 0 staat
 
 var now,
     dt   = 0,
@@ -57,12 +58,12 @@ function processInput() {
 	if(keyboard.pressed("right")) camera.rotation.y-= Math.PI * 1 / 180;
  }
  
-function addCube(x, y, z, clr, xp, zp) {
+function addCube(x, y, z, tex, xp, yp, zp) {
 	var cubeGeometry = new THREE.CubeGeometry(x, y, z);	
-	var cubeMaterial = new THREE.MeshLambertMaterial({ map:texture, side:THREE.DoubleSide });
+	var cubeMaterial = new THREE.MeshLambertMaterial({ map:tex, side:THREE.DoubleSide });
 	var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);	// maak een cube
 	cube.position.x = xp;
-	cube.position.y = y/2;
+	cube.position.y = y/2+yp;
 	cube.position.z = zp;
 	cube.rotation.y = Math.PI * 15 / 180;
 	scene.add(cube);

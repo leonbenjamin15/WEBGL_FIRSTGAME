3feaf10e337e7bbb65f522d71735b162963a5062
leonbenjamin15 <leonbenjamin15@hotmail.com>
2014-04-20 15:41:25 +0200

    betere collision detection
    yayyy

--- js/includes/functions.js
+++ js/includes/functions.js
@@ -27,6 +27,7 @@ function addCamera(width, height){
  * wordt gebruikt bij elke druk de keyboard
  */
 function processInput() {
+	
     if(keyboard.pressed("up")) {
         camera.position.z -= Math.cos(camera.rotation.y);
         camera.position.x -= Math.sin(camera.rotation.y);
@@ -99,13 +100,34 @@ function addCube(x, y, z, img, rx, ry, xp, yp, zp) {
 
 function detectCollision(xpos, zpos, depth, width) {
 	var playerSize = 5;
-	var x1 = camera.position.x <(xpos+width/2+playerSize)
-	var x2 = camera.position.x>(xpos-width/2-playerSize)
-	var z1 = camera.position.z<(zpos+depth/2+playerSize)
-	var z2 = camera.position.z>(zpos-depth/2)-playerSize
-	if( x1 && x2 && z1 && z2 ) {
-			//camera.position.x = 0;
-			camera.position.z = zpos+depth/2+playerSize;
+	var x1 = xpos+width/2+playerSize;
+	var x2 = xpos-width/2-playerSize;
+	var z1 = zpos+depth/2+playerSize;
+	var z2 = zpos-depth/2-playerSize;
+	
+	
+	if( camera.position.x < x1 && camera.position.x > x2 && camera.position.z < z1 && camera.position.z > z2) {
+		var d1 = Math.abs((x1 - camera.position.x));
+		var d2 = Math.abs((camera.position.x - x2));
+		var d3 = Math.abs((z1 - camera.position.z));
+		var d4 = Math.abs((camera.position.z - z2));
+		var max = Math.min(d1, d2, d3, d4);
+		
+			switch(max) 
+			{
+				case d1:
+					camera.position.x = x1;
+					break;
+				case d2:
+					camera.position.x = x2;
+					break;					
+				case d3:
+					camera.position.z = z1;
+					break;					
+				case d4:
+					camera.position.z = z2;
+					break;
+			}
 		}
 }
 /**
